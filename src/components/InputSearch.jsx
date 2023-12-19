44b8422579240a91ec61677c69262f37e3d99e7c
Dergi Samayoa <dergio@gmail.com>
2023-12-19 13:18:49 -0600

    Cambio search para busqueda por botón y no al teclear las letras en el input

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -25,7 +25,7 @@ const InputSearch = () => {
   return (
     <form onSubmit={handleSubmit}
           className="w-[400px] h-[40px] rounded-md  flex justify-between shadow-[2px_2px_50px_0_rgba(55,71,79,0.2)] border-[1px] border-[#eee] max-sm:w-auto">
-      <input onChange={handleChange} className="rounded-xl px-4 w-[300px] outline-none max-sm:w-[250px]" type="text" name="name" placeholder="Search your pokemon!" autoComplete="off"/>
+      <input className="rounded-xl px-4 w-[300px] outline-none max-sm:w-[250px]" type="text" name="name" placeholder="Search your pokemon!" autoComplete="off"/>
       <button className="w-[80px] h-full bg-[#cc0000] rounded-md text-white">Search</button>
     </form>
   )
